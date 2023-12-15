3d878d0c844676009da5854c5bcd7008f1b58b5c
jrbistuer <jrbistuer@gmail.com>
2023-12-15 16:33:39 +0100

    auth

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule]
+})
+export class LoginPage implements OnInit {
+
+  credentials!: FormGroup;
+
+  constructor(private fb: FormBuilder, 
+    private authService: AuthService,
+		private router: Router) { }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.credentials = this.fb.group({
+			email: ['', [Validators.required, Validators.email]],
+			password: ['', [Validators.required, Validators.minLength(6)]]
+		});
+  }
+
+  async register() {
+
+		const user = await this.authService.register(this.credentials.value);
+
+		if (user) {
+			this.router.navigateByUrl('/', { replaceUrl: true });
+		} else {
+			console.log('Registration failed, Please try again!');
+		}
+	}
+
+	async login() {
+
+		const user = await this.authService.login(this.credentials.value);
+
+		if (user) {
+			this.router.navigateByUrl('/', { replaceUrl: true });
+		} else {
+			console.log('Login failed, Please try again!');
+		}
+	}
+
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor(private authService: AuthService,
+    private router: Router) {}
+
+  logout() {
+    this.authService.logout().then(() => {
+      this.router.navigateByUrl('/login');
+    })
+  }
 }
