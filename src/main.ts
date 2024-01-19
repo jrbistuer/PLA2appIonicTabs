8b924035bd94ee8a35ecc1eccdbb5f7da42a08a0
jrbistuer <jrbistuer@gmail.com>
2024-01-19 12:50:47 +0100

    pwa

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom, isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withComponentInputBinding } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
@@ -10,6 +10,7 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideServiceWorker } from '@angular/service-worker';
 
 if (environment.production) {
   enableProdMode();
@@ -20,10 +21,14 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes),
-    importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"testcifo01","appId":"1:615577967112:web:a29daf48e65aaffb820fbd","storageBucket":"testcifo01.appspot.com","apiKey":"AIzaSyDw82D8pnvYAjf_1AQ2AXWZdkIv_onSNws","authDomain":"testcifo01.firebaseapp.com","messagingSenderId":"615577967112"}))),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({ "projectId": "testcifo01", "appId": "1:615577967112:web:a29daf48e65aaffb820fbd", "storageBucket": "testcifo01.appspot.com", "apiKey": "AIzaSyDw82D8pnvYAjf_1AQ2AXWZdkIv_onSNws", "authDomain": "testcifo01.firebaseapp.com", "messagingSenderId": "615577967112" }))),
     importProvidersFrom(provideAuth(() => getAuth())),
     importProvidersFrom(provideFirestore(() => getFirestore())),
     importProvidersFrom(provideStorage(() => getStorage())),
-    provideRouter(routes, withComponentInputBinding())
-  ],
+    provideRouter(routes, withComponentInputBinding()),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 });
